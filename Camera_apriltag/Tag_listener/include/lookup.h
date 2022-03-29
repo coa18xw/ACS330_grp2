2492b4e54847c83102aea7f1a0f15d08085bccb7
uos <jwrobson1@sheffield.ac.uk>
2022-03-29 14:05:07 +0100

    added other tags

--- Camera_apriltag/Tag_listener/include/lookup.h
+++ Camera_apriltag/Tag_listener/include/lookup.h
@@ -1,12 +1,18 @@
 
 const char* lookup(int32_t Id)
 {
-	int size = 3;
+	int size = 8;
 	int id = Id;
 	const char* table[size];
 	table[0] = "Base";
 	table[1] = "143.167.45.210";
 	table[2] = "143.167.47.142";
+	table[3] = "Base";
+	table[4] = "143.167.45.210";
+	table[5] = "143.167.47.142";
+	table[6] = "6_Ip";
+	table[7] = "143.167.45.210";
+	table[8] = "143.167.47.142";
 	if (id>size)
 	{
 		return "error - tag not known";
