4aa9346d168335d0c2ad1a8ec26a8630da4b4709
Robson-j-w <jwrobson1@sheffield.ac.uk>
2022-04-19 15:43:39 -0700

    added new idea to ll_client_controller

--- low_level_controller/src/ll_client_controller.cpp
+++ low_level_controller/src/ll_client_controller.cpp
@@ -183,6 +183,172 @@ ROS_INFO("%s controller: running", controller_name_.c_str());
  }
 };
 
+//maybe try splitting into two classes input and output
+/*
+
+class LLController
+{
+protected:
+
+	ros::NodeHandle nh_;
+	actionlib::SimpleActionServer<low_level_controller::ll_client_serverAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+	std::string controller_name_;
+	// create messages that are used to published feedback/result
+	low_level_controller::ll_client_serverFeedback feedback_;
+	low_level_controller::ll_client_serverResult result_;
+
+public:
+LLController(std::string controller_id, int argc,char** argv) :
+as_(nh_, controller_id, boost::bind(&LLController::executeCB, this, _1), false), controller_name_(controller_id)
+	{
+		as_.start();
+		ROS_INFO("%s activated",controller_id.c_str());
+		ros::init(argc, argv, "process");
+	}
+
+~LLController(void)
+{
+}
+void executeCB(const low_level_controller::ll_client_serverGoalConstPtr &goal)
+ {
+//llserver
+	bool success = true;
+	
+	int32_t size = sizeof(goal->task);
+	
+	//spin
+	boost::thread spin_thread(&spinThread);
+	int i = 0;
+	while(goal->task[i].c_str() != NULL)
+  {
+	//displaying which controller running
+ROS_INFO("%s controller: running", controller_name_.c_str());
+//excecuting called action
+	//action client stuff
+		
+		//process stations
+				if(goal->task[i] == "Heating"||goal->task[i]  == "Cleaning"||goal->task[i]  == "Cutting")
+				{
+					
+					actionlib::SimpleActionClient<process_actions::processAction> ac("process", true);
+					boost::thread spin_thread(&spinThread);
+					ROS_INFO("Waiting for action server to start.");
+					ac.waitForServer(); //will wait for infinite time
+					//
+					ROS_INFO("Action server started, sending goal : %s",goal->task[i].c_str());
+					process_actions::processGoal Goal;
+					Goal.station_id = goal->task[i].c_str();
+
+
+					ac.sendGoal(Goal);
+					bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0)); // 0 = infinite timeout
+					if (finished_before_timeout)
+					{
+					actionlib::SimpleClientGoalState state = ac.getState();
+					ROS_INFO("Action finished: %s",state.toString().c_str());
+					}
+					else
+					ROS_INFO("Action did not finish before the time out.");
+				}
+
+				
+		//moblie platforms placeholders will do a task per location they need to go to
+				else if(goal->task[i]  == "MM1")
+				{
+					actionlib::SimpleActionClient<mm_actions::mmAction> ac1("mm1", true);
+					boost::thread spin_thread(&spinThread);
+					ROS_INFO("Waiting for action server to start.");
+					ac1.waitForServer(); //will wait for infinite time
+					//
+					ROS_INFO("Action server started, sending goal : %s",goal->task[i].c_str());
+					mm_actions::mmGoal Goal;
+					Goal.location = {12,14}; // to be changed 
+
+					ac1.sendGoal(Goal);
+					bool finished_before_timeout = ac1.waitForResult(ros::Duration(30.0)); // 0 = infinite timeout
+					if (finished_before_timeout)
+					{
+					actionlib::SimpleClientGoalState state = ac1.getState();
+					ROS_INFO("Action finished: %s",state.toString().c_str());
+					}
+					else
+					ROS_INFO("Action did not finish before the time out.");
+				}
+				else if(goal->task[i] == "MM2")
+				{
+					actionlib::SimpleActionClient<mm_actions::mmAction> ac2("mm2", true);
+					boost::thread spin_thread(&spinThread);
+					ROS_INFO("Waiting for action server to start.");
+					ac2.waitForServer(); //will wait for infinite time
+					//
+					ROS_INFO("Action server started, sending goal : %s",goal->task[i].c_str());
+					mm_actions::mmGoal Goal;
+					Goal.location = {12,14}; // to be changed 
+
+					ac2.sendGoal(Goal);
+					bool finished_before_timeout = ac2.waitForResult(ros::Duration(30.0)); // 0 = infinite timeout
+					if (finished_before_timeout)
+					{
+					actionlib::SimpleClientGoalState state = ac2.getState();
+					ROS_INFO("Action finished: %s",state.toString().c_str());
+					}
+					else
+					ROS_INFO("Action did not finish before the time out.");
+				}
+		//Assembly stations
+				else if(goal->task[i]  == "Assembly")
+				{
+					actionlib::SimpleActionClient<assembly_actions::assemblyAction> ac("assembly", true);
+					boost::thread spin_thread(&spinThread);
+					ROS_INFO("Waiting for action server to start.");
+					ac.waitForServer(); //will wait for infinite time
+					//
+					ROS_INFO("Action server started, sending goal : %s",goal->task[i].c_str());//.c_str()
+					assembly_actions::assemblyGoal Goal;
+					Goal.order = {"r","g","b"}; // need to add a way to  read and send the ordered arragment
+
+
+					ac.sendGoal(Goal);
+					bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0)); // 0 = infinite timeout
+					if (finished_before_timeout)
+					{
+					actionlib::SimpleClientGoalState state = ac.getState();
+					ROS_INFO("Action finished: %s",state.toString().c_str());
+
+					}
+					else
+					ROS_INFO("Action did not finish before the time out.");
+				}
+		//checks
+					else
+					{
+					spin_thread.join();
+					break;
+					}
+					
+//common
+					
+				
+
+		
+		i++;
+	}
+	//end of action client stuff
+  
+// closing action_server
+	if(true)
+	{
+	result_.complete = true;
+	//ROS_INFO("%s task completed",goal->task[i].c_str());
+	as_.setSucceeded(result_);	
+	}		
+	//closing thread from above
+		
+  
+ }
+};
+*/
+
 
 
  int main(int argc, char** argv)
